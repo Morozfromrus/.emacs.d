5e099288bb4799e8ec6622ff1b348aeda2957a31
Andrey Morozov <Morozfromrus@mail.ru>
2015-11-11 02:37:58 +0300

    more

---
+++ custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("8f3087055417c3087900575adbcc4f7e8fc3b1349bcfe0d4bc799392bcd4e990" "9527feeeec43970b1d725bdc04e97eb2b03b15be982ac50089ad223d3c6f2920" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
---
+++ settings/appearance.el
@@ -0,0 +1,15 @@
+;; simplifies the interface
+(menu-bar-mode -1)
+(toggle-scroll-bar -1)
+(tool-bar-mode -1)
+
+;; Highlight current line
+(global-hl-line-mode 1)
+
+;; Highlight matching parentheses when the point is on them.
+(show-paren-mode 1)
+
+;; Theme
+(load-theme 'default-black)
+
+(provide 'appearance)
