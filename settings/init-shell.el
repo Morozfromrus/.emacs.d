b41f710cddeedb254fc93e5b179795e0c1fd0d65
mad <Morozfromrus@mail.ru>
2015-12-18 18:32:51 +0300

    max shell output len

--- settings/init-shell.el
+++ settings/init-shell.el
@@ -1,9 +1,12 @@
-(require 'em-tramp)
+;; (require 'em-tramp)
 
-(setq eshell-prefer-lisp-functions t)
-(setq eshell-prefer-lisp-variables t)
+;; (setq eshell-prefer-lisp-functions t)
+;; (setq eshell-prefer-lisp-variables t)
 
-(setq password-cache t)
-(setq password-cache-expiry 3600)
+;; (setq password-cache t)
+;; (setq password-cache-expiry 3600)
+
+(add-hook 'comint-output-filter-functions
+          'comint-truncate-buffer)
 
 (provide 'init-shell)
