b998e53f3a0a3b344cb22644ca3fe952879ab6a0
Андрей Морозов <merithon@mail.ru>
2016-01-27 15:47:40 +0300

    fix bug

--- settings/init-expand-region.el
+++ settings/init-expand-region.el
@@ -1,6 +1,6 @@
 (use-package expand-region
   :ensure t
   :commands (er/expand-region er/mark-inside-quotes)
-  :bind (("M-r" . er/expand-rexgion)))
+  :bind (("M-r" . er/expand-region)))
 
 (provide 'init-expand-region)
