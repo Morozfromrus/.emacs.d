ff7f340bdd223ad6b560661d1cb73047041aa390
Андрей Морозов <merithon@mail.ru>
2016-02-05 17:12:49 +0300

    magit + note

--- settings/init-magit.el
+++ settings/init-magit.el
@@ -1,5 +1,7 @@
 (use-package magit
   :ensure t
-  :config (global-set-key (kbd "C-x g") 'magit-status))
+  :config
+  (global-set-key (kbd "C-x g") 'magit-status)
+  (setq magit-completing-read-function 'magit-ido-completing-read))
 
 (provide 'init-magit)
