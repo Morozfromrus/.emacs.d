b21678efd28bd03e6c42038031c62ce02bf02602
mad <Morozfromrus@mail.ru>
2016-03-02 00:20:55 +0300

    python tests

--- settings/init-python-mode.el
+++ settings/init-python-mode.el
@@ -1,33 +1,11 @@
 (use-package python-django
   :ensure t)
 
-(use-package python
-  :ensure t
-  :init
-  (progn
-    (setq
-     gud-pdb-command-name "ipdb"
-     python-shell-interpreter "ipython"
-     python-shell-interpreter-args "-i --autoindent"
-     python-shell-prompt-regexp "In \\[[0-9]+\\]: "
-     python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
-     python-shell-completion-setup-code
-     "import rlcompleter2
-rlcompleter2.setup()
-from IPython.core.completerlib import module_completion"
-     python-shell-completion-module-string-code
-     "';'.join(module_completion('''%s'''))\n"
-     python-shell-completion-string-code
-     "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
-    (when (fboundp 'jedi:setup)
-      (add-hook 'python-mode-hook 'jedi:setup))))
+(use-package python-mode
+  :ensure t)
 
-(defun python-add-breakpoint ()
-  "Add a break point"
-  (interactive)
-  (insert "import ipdb; ipdb.set_trace()")
-  (highlight-lines-matching-regexp "^[ ]*import ipdb; ipdb.set_trace()"))
-
-(define-key python-mode-map (kbd "C-c C-b") 'python-add-breakpoint)
+(autoload 'python-mode "python-mode" "Python Mode." t)
+(add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
+(add-to-list 'interpreter-mode-alist '("python" . python-mode))
 
 (provide 'init-python-mode)
