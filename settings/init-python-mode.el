162fd7cf8647ddbd9d878a73a2f6b98313f38f7d
mad <Morozfromrus@mail.ru>
2016-03-02 02:07:28 +0300

    python back

--- settings/init-python-mode.el
+++ settings/init-python-mode.el
@@ -1,11 +1,48 @@
 (use-package python-django
   :ensure t)
 
-(use-package python-mode
+(use-package python
   :ensure t)
 
-(autoload 'python-mode "python-mode" "Python Mode." t)
-(add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
-(add-to-list 'interpreter-mode-alist '("python" . python-mode))
+(setq
+ python-shell-interpreter "ipython"
+ python-shell-interpreter-args "--gui=wx --matplotlib=wx --colors=Linux"
+ python-shell-prompt-regexp "In \\[[0-9]+\\]: "
+ python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
+ python-shell-completion-setup-code
+   "from IPython.core.completerlib import module_completion"
+ python-shell-completion-module-string-code
+   "';'.join(module_completion('''%s'''))\n"
+ python-shell-completion-string-code
+ "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
+
+(use-package jedi
+  :ensure t)
+(setq jedi:setup-keys t)
+(autoload 'jedi:setup "jedi" nil t)
+(add-hook 'python-mode-hook 'jedi:setup)
+
+;; ------------------
+;; misc python config
+;; ------------------
+
+; Set PYTHONPATH, because we don't load .bashrc
+(defun set-python-path-from-shell-PYTHONPATH ()
+  (let ((path-from-shell (shell-command-to-string "$SHELL -i -c 'echo $PYTHONPATH'")))
+    (setenv "PYTHONPATH" path-from-shell)))
+
+(if window-system (set-python-path-from-shell-PYTHONPATH))
+
+(setq auto-mode-alist
+      (append 
+       (list '("\\.pyx" . python-mode)
+             '("SConstruct" . python-mode))
+       auto-mode-alist))
+
+; keybindings
+(eval-after-load 'python
+  '(define-key python-mode-map (kbd "C-c !") 'python-shell-switch-to-shell))
+(eval-after-load 'python
+  '(define-key python-mode-map (kbd "C-c |") 'python-shell-send-region))
 
 (provide 'init-python-mode)
