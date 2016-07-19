1eaa89d7cee775c46aae9cc6fe64624c1327be2e
Андрей Морозов <merithon@mail.ru>
2016-07-19 17:28:56 +0300

    add flycheck

--- init.el
+++ init.el
@@ -29,6 +29,14 @@
   :init (progn
 	  (setq use-package-verbose t)))
 
+(use-package flycheck
+  :config (progn
+            (add-hook 'after-init-hook 'global-flycheck-mode)
+            (setq flycheck-check-syntax-automatically '(save idle-change mode-enabled)
+                  flycheck-idle-change-delay 0.8
+                  flycheck-display-errors-function #'flycheck-display-error-messages-unless-error-list))
+  :ensure t)
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Configuration for emacs
 
